7b89bbe2c80447ee06fd89866786f24ec4a2d5e4
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-09-11 15:51:39 -0500

    ♻️🌌
    +-

--- plugins/yt-audio.js
+++ plugins/yt-audio.js
@@ -3,7 +3,7 @@
 **/
 let handler = async (m, { conn, command, text }) => {
 	if (!text) return m.reply(`Que desea buscar en Youtube?, Ejemplo de uso: \n\n${Prefijo + command} mtc s3rl`)
-        let name = await conn.getName(m.sender)
+	let name = await conn.getName(m.sender)
 	let mcarga = m.reply(`_Procesando, ${name} por favor espere..._`)
 	await mcarga
 	let playtext = encodeURIComponent(text)
@@ -11,7 +11,7 @@ try {
 	let myapidl = await fetchJson(`https://latam-api.vercel.app/api/ytplay?apikey=${MyApiKey}&q=${playtext}`)
 	let ytthumb = await getBuffer(myapidl.logo)
 	await conn.sendMessage(m.chat, {text: `🔍 Resultado encontrado para: ${text}\n✍️ Titulo: ${myapidl.titulo}\n⏳ Duracion: 1:23 ━━━━●───────── ${myapidl.duracion}\n👀 Vistas: ${myapidl.vistas}\n📝 Autor: ${myapidl.autor}\n📜 Descripción: ${myapidl.descripcion}\n⛓️ URL: ${myapidl.link}\n\n\`\`\`Enviando audio, espere...\`\`\``}, {quoted: m })
-	conn.sendMessage(m.chat, { audio: { url: myapidl.descarga }, mimetype: 'audio/mpeg', fileName: `${myapidl.titulo}.mp3`, contextInfo:{"externalAdReply":{"title": `${myapidl.titulo}`,"body": ``,"previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": ytthumb,"sourceUrl": myapidl.descarga }} }, { quoted: m }).catch(e=>{m.reply(`Ocurrio un error, use el comando:\n\n${Prefijo}audio2 ${text}\n`)})
+	conn.sendMessage(m.chat, { audio: { url: myapidl.descarga }, mimetype: 'audio/mpeg', fileName: `${myapidl.titulo}.mp3`, contextInfo:{"externalAdReply":{"title": `${myapidl.titulo}`,"body": ``,"previewType": "PHOTO","thumbnailUrl": ``,"thumbnail": ytthumb,"sourceUrl": myapidl.descarga }} }, { quoted: m }).catch(e=>{m.reply(`[ ! ] Ocurrio un error inesperado... u.u`)})
 } catch (e) {
 m.reply(`[ ! ] Error, vuelva a intentarlo mas tarde...`)
 }
